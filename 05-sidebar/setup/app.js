2e225ff5fafe292bb4ea69b04f734c0517985995
jukirchn <jkirchner@mail.de>
2022-02-17 20:19:23 +0100

    added solution of project 5 - sidebar

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector('.sidebar-toggle')
+const closeBtn = document.querySelector('.close-btn')
+const sidebar = document.querySelector('.sidebar')
+
+toggle.addEventListener('click', function() {
+    // console.log(sidebar.classList);
+    sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click', function() {
+    sidebar.classList.remove("show-sidebar")
+})
