888b13922b43a7fc8d1c5cbae6d16ec3ebb5d4eb
jukirchn <jkirchner@mail.de>
2022-02-21 19:37:14 +0100

    added solution of project 11 - tabs

---
+++ 11-tabs/setup/app.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener('click', function(e) {
+    // console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    if (id) { // if id exists
+        // remove active from all buttons
+        btns.forEach(function(btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // hide other articles
+        articles.forEach(function(article) {
+            article.classList.remove("active");
+        })
+        element = document.getElementById(id);
+        element.classList.add("active");
+    }
+})
